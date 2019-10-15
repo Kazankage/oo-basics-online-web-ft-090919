8f473860ffb8d1181cc4caf865cd00996ab1167b
Randel Jiao <randeljiao@gmail.com>
2019-10-15 14:56:53 +0000

    Automatically backed up by Learn

---
+++ ger.rb
@@ -0,0 +1,13 @@
+class Book 
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  attr_accessor :title, :author, :page_count, :genre
+  
+  def turn_page
+    puts "Flipping the page...wow, you read really fast"
+  end
+  
+end
